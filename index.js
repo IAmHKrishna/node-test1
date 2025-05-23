a778f09f2ce70bbfaaa773f92538d64fdec8ca3a
hare.krishna <hare.krishna@terotam.com>
2025-05-24 00:00:50 +0530

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"application/json"})
+
+    res.end(JSON.stringify({ name: "hello world" }));
+})
+
+server.listen(3000,()=>{
+    console.log("server is running on 30000")
+})
