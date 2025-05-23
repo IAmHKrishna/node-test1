d22b42daa0c160013e80af319fc2287ea4affa68
hare.krishna <hare.krishna@terotam.com>
2025-05-24 02:22:02 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http")
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"application/json"})
 
-    res.end(JSON.stringify({ name: "hello world12" }));
+    res.end(JSON.stringify({ name: "hello world123" }));
 })
 
 // server.listen(80)
